675eb079e154ad6832d2039d4616cd07c06ac556
duckien <nguyenduckien0162@gmail.com>
2025-02-19 00:08:20 +0700

    kimnnguu

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const contentLetterSrart_actived = "Hãy mở ra nhie ..." //Lời mở đầu cho bức thư
-const mainContentLetter = "Nhân dịp ngày lành tháng tốt. Trân trọng kính mời bạn Dũng và người thương đến dự buổi trao bằng tốt nghiệp của tôi tại hội trường G3 trường Đại học Xây Dựng Hà Nội vào lúc 8h ngày 01/03/2025(Tức ngày 02 tháng 02 năm Ất Tỵ). Sự hiện diện của bạn là niềm vinh hạnh của tôi " 
+const mainContentLetter = "Nhân dịp ngày lành tháng tốt. Trân trọng kính mời 105 Kim Ngưu đến dự buổi trao bằng tốt nghiệp của tôi tại hội trường G3 trường Đại học Xây Dựng Hà Nội vào lúc 8h ngày 01/03/2025(Tức ngày 02 tháng 02 năm Ất Tỵ). Sự hiện diện của mọi người là niềm vinh hạnh của tôi " 
 //Nội dung của bức thư
 
 // Gắn 1 đường link ảnh bất kì
@@ -9,7 +9,7 @@ imgStart.src = "./img/IMG_4701-removebg-preview.png";
 
 // Gắn 1 link ảnh bất kì
 let imgLetter = document.querySelector(".img");
-imgLetter.src = "./img/dung-removebg-preview.png"; //Hình ảnh xuất hiện trong nội dung của bức thư sau khi bức thư được viết ra hết
+imgLetter.src = "./img/IMG_1810-removebg-preview.png"; //Hình ảnh xuất hiện trong nội dung của bức thư sau khi bức thư được viết ra hết
 
 const splitContentLetterSrart_actived = contentLetterSrart_actived.split("");
 
